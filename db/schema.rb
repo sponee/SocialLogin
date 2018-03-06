6d8177b756dba43a38cfe6d06f83aa6171d95e6d
sponee <kyle.sponheim@gmail.com>
2018-03-05 21:29:34 -0800

    disallow null values in identities

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20180306023209) do
 
   create_table "identities", force: :cascade do |t|
-    t.string "uid"
-    t.string "provider"
-    t.integer "user_id"
+    t.string "uid", null: false
+    t.string "provider", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_identities_on_user_id"
