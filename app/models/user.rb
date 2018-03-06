68f163a7962b8578ac595a357171fb474af05fba
sponee <kyle.sponheim@gmail.com>
2018-03-05 19:04:04 -0800

    create the user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
-# TODO give this model a table
-class User
-  include ActiveModel::Model
-  attr_accessor :name, :location, :image_url, :url
+class User < ApplicationRecord
+  has_many :identities
   
   def self.from_omniauth(auth_hash)
-    user = User.new(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    user = User.find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
     user.name = auth_hash['info']['name']
     user.location = auth_hash['info']['location']
     user.image_url = auth_hash['info']['image']
